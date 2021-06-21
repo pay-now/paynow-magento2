befaf0f1d0c3269d6410892d33033eb2de8a91a2
Emil Leszczak <emil.leszczak@falest.pl>
2021-06-21 14:13:28 +0200

    Add refund support

---
+++ Gateway/Request/Refund/RefundRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Paynow\PaymentGateway\Gateway\Request\Refund;
+
+use Magento\Payment\Gateway\Request\BuilderInterface;
+use Paynow\PaymentGateway\Helper\PaymentField;
+use Paynow\PaymentGateway\Helper\PaymentHelper;
+use Paynow\PaymentGateway\Helper\RefundField;
+
+class RefundRequest implements BuilderInterface
+{
+    /**
+     * @var PaymentHelper
+     */
+    private $paymentHelper;
+
+    public function __construct(PaymentHelper $paymentHelper)
+    {
+        $this->paymentHelper = $paymentHelper;
+    }
+
+    /**
+     * @param array $buildSubject
+     * @return array
+     */
+    public function build(array $buildSubject)
+    {
+        /** @var PaymentDataObject $paymentDataObject */
+        $paymentDataObject = SubjectReader::readPayment($buildSubject);
+        $refundAmount = SubjectReader::readAmount($buildSubject);
+        $order = $paymentDataObject->getOrder();
+        $referenceId = $order->getOrderIncrementId();
+
+        $request['body'] = [
+            RefundField::AMOUNT_FIELD_NAME => $this->paymentHelper->formatAmount($refundAmount)
+        ];
+
+        $request['headers'] = [
+            PaymentField::IDEMPOTENCY_KEY_FIELD_NAME => uniqid($referenceId, true)
+        ];
+
+        return $request;
+    }
+}
