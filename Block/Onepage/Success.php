b59de5cef21d0e3294322c3345eee5aa3f44c705
Michał Składanowski <michal.skladanowski@melements.pl>
2021-09-30 15:14:33 +0200

    Get payment status on succes page

--- Block/Onepage/Success.php
+++ Block/Onepage/Success.php
@@ -6,10 +6,17 @@ use Magento\Checkout\Block\Onepage\Success as MagentoSuccess;
 use Magento\Checkout\Model\Session;
 use Magento\Framework\App\Http\Context as AppContext;
 use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Phrase;
 use Magento\Framework\View\Element\Template\Context;
 use Magento\Sales\Model\Order;
 use Magento\Sales\Model\Order\Config;
+use Magento\Sales\Model\OrderFactory;
+use Paynow\Model\Payment\Status;
+use Paynow\PaymentGateway\Helper\NotificationProcessor;
+use Paynow\PaymentGateway\Helper\PaymentField;
 use Paynow\PaymentGateway\Helper\PaymentHelper;
+use Paynow\PaymentGateway\Model\Logger\Logger;
+use Paynow\Service\Payment;
 
 /**
  * Class Success
@@ -19,7 +26,7 @@ use Paynow\PaymentGateway\Helper\PaymentHelper;
 class Success extends MagentoSuccess
 {
     /**
-     * @var OrderInterfaceFactory
+     * @var Magento\Sales\Model\OrderFactory
      */
     private $orderFactory;
 
@@ -28,33 +35,61 @@ class Success extends MagentoSuccess
      */
     private $paymentHelper;
 
+    /**
+     * @var Logger
+     */
+    private $logger;
+
+    /**
+     * @var NotificationProcessor
+     */
+    private $notificationProcessor;
+    /**
+     * @var Order
+     */
+    private $order;
+    /**
+     * @var string
+     */
+    private $status;
+
     public function __construct(
         Context $context,
         Session $checkoutSession,
         Config $orderConfig,
         AppContext $httpContext,
+        OrderFactory  $orderFactory,
         PaymentHelper $paymentHelper,
-        OrderInterfaceFactory $orderFactory,
+        Logger $logger,
+        NotificationProcessor $notificationProcessor,
         array $data = []
     ) {
         parent::__construct($context, $checkoutSession, $orderConfig, $httpContext, $data);
-        $this->paymentHelper = $paymentHelper;
         $this->orderFactory = $orderFactory;
-    }
-
-    public function  getOrder()
-    {
-        return $this->orderFactory->create()->load($this->getLastOrderId());
+        $this->paymentHelper = $paymentHelper;
+        $this->logger = $logger;
+        $this->notificationProcessor = $notificationProcessor;
+        $this->order = $this->_checkoutSession->getLastRealOrder();
+        $this->retrievePaymentStatusAndUpdateOrder();
     }
 
     /**
-     *
-     * @return string
+     * @return Order
+     */
+    public function getOrder()
+    {
+        return $this->orderFactory->create()->loadByIncrementId($this->getData('order_id'));
+    }
+
+
+    /**
+     * @return bool
      * @throws NoSuchEntityException
      */
-    public function canRetryPayment(): string
+    public function canRetryPayment(): bool
     {
-        return $this->paymentHelper->isRetryPaymentActiveForOrder($this->getOrder());
+        $order = $this->getOrder();
+        return $this->paymentHelper->isRetryPaymentActiveForOrder($order);
     }
 
     /**
@@ -64,6 +99,43 @@ class Success extends MagentoSuccess
      */
     public function getRetryPaymentUrl(): string
     {
-        return $this->paymentHelper->getRetryPaymentUrl($this->getLastOrderId());
+        return $this->paymentHelper->getRetryPaymentUrl($this->order->getEntityId());
+    }
+
+    /**
+     * @return Phrase
+     */
+    public function getPaymentStatusPhrase()
+    {
+        switch ($this->status) {
+            case Status::STATUS_REJECTED:
+                return __('Your payment has been rejected.');
+            case Status::STATUS_ERROR:
+                return __('An error occurred during your payment process.');
+            case Status::STATUS_NEW:
+            case Status::STATUS_PENDING:
+                return __('Your payment process has not been completed.');
+            case Status::STATUS_CONFIRMED:
+                return __('Your payment has been completed.');
+        }
+    }
+
+    private function retrievePaymentStatusAndUpdateOrder()
+    {
+        $paymentId = $this->order->getPayment()->getAdditionalInformation(PaymentField::PAYMENT_ID_FIELD_NAME);
+        $loggerContext = [PaymentField::PAYMENT_ID_FIELD_NAME => $paymentId];
+        try {
+            $service = new Payment($this->paymentHelper->initializePaynowClient());
+            $apiResponseObject = $service->status($paymentId);
+            $this->status = $apiResponseObject->getStatus();
+            $this->logger->debug(
+                "Retrieved status response",
+                array_merge($loggerContext, [$this->status])
+            );
+            $this->notificationProcessor->process($paymentId, $this->status, $this->order->getIncrementId());
+
+        } catch (\Exception $exception) {
+            $this->logger->error($exception->getMessage(), $loggerContext);
+        }
     }
 }
