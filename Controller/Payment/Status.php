0dc3dc713b9e5a983b24de2d0bffe2f47ca7c33b
Emil Leszczak <emil.leszczak@falest.pl>
2021-12-06 09:33:17 +0100

    Status controller

--- Controller/Payment/Status.php
+++ Controller/Payment/Status.php
@@ -2,29 +2,52 @@
 
 namespace Paynow\PaymentGateway\Controller\Payment;
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderFactory;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Paynow\PaymentGateway\Helper\PaymentField;
 
-
-/**
- * Class Status
- *
- * @package Paynow\PaymentGateway\Controller\Payment
- */
 class Status extends Action
 {
+    /**
+     * @var Magento\Sales\Model\OrderFactory
+     */
+    private $orderFactory;
+
+    /**
+     * @var JsonFactory
+     */
     private $resultJsonFactory;
 
-    public function __construct(JsonFactory $resultJsonFactory, Context $context)
+    public function __construct(Context $context, OrderFactory $orderFactory, JsonFactory $resultJsonFactory)
     {
-        parent::__construct($context);
+        $this->orderFactory = $orderFactory;
         $this->resultJsonFactory = $resultJsonFactory;
+        parent::__construct($context);
     }
 
     public function execute()
     {
-        $resultJson = $this->resultJsonFactory->create();
-        return $resultJson->setData([['title' => 'title', 'content' => 'content']]);
+        $result = $this->resultJsonFactory->create();
+        $orderId = (int)$this->getRequest()->getParams()['order_id'];
+        /** @var Order */
+        $order = $this->orderFactory->create()->loadByIncrementId($orderId);
+
+        if (!$order->getId()) {
+            return $result->setStatusHeader(404);
+        }
+
+        $paymentAdditionalInformation = $order->getPayment()->getAdditionalInformation();
+        $orderPaymentStatus = $paymentAdditionalInformation[PaymentField::STATUS_FIELD_NAME];
+
+//        if ($this->getRequest()->isAjax()) {
+            $data = [
+                'order_status'   => $order->getStatus(), //getStatusLabel()
+                'payment_status' => $orderPaymentStatus
+            ];
+            return $result->setData($data);
+//        }
+
+//        return null;
     }
 }
