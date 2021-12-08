216cb261a30ab152e34a14614e00d15d37e82824
Michał Składanowski <michal.skladanowski@melements.pl>
2021-12-08 10:02:51 +0100

    Merge

--- Controller/Payment/Status.php
+++ Controller/Payment/Status.php
@@ -5,7 +5,9 @@ namespace Paynow\PaymentGateway\Controller\Payment;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
-
+use Magento\Sales\Model\Order;
+use Magento\Sales\Model\OrderFactory;
+use Paynow\PaymentGateway\Helper\PaymentField;
 
 /**
  * Class Status
@@ -14,17 +16,41 @@ use Magento\Framework\Controller\Result\JsonFactory;
  */
 class Status extends Action
 {
+    /**
+     * @var JsonFactory
+     */
     private $resultJsonFactory;
 
-    public function __construct(JsonFactory $resultJsonFactory, Context $context)
+    /**
+     * @var OrderFactory
+     */
+    private $orderFactory;
+
+    public function __construct(OrderFactory $orderFactory, JsonFactory $resultJsonFactory, Context $context)
     {
         parent::__construct($context);
+        $this->orderFactory = $orderFactory;
         $this->resultJsonFactory = $resultJsonFactory;
     }
 
     public function execute()
     {
         $resultJson = $this->resultJsonFactory->create();
-        return $resultJson->setData([['title' => 'title', 'content' => 'content']]);
+        $id_order = $this->getRequest()->getParam('id_order');
+
+        /** @var Order */
+        $order = $this->orderFactory->create()->loadByIncrementId($id_order);
+        if (!$order->getId()) {
+            return $resultJson->setStatusHeader(404, null, 'Not Found');
+        }
+
+        $paymentAdditionalInformation = $order->getPayment()->getAdditionalInformation();
+        $paymentStatus = $paymentAdditionalInformation[PaymentField::STATUS_FIELD_NAME] ?? null;
+
+        return $resultJson->setData([
+            'order_status'   => $order->getStatus(),
+            'order_status_label'   => $order->getStatusLabel(),
+            'payment_status' => $paymentStatus
+        ]);
     }
 }
