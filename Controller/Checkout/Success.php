beb081926f68bbce9c76ef4fac4bc48d58b91712
Michał Składanowski <michal.skladanowski@melements.pl>
2021-12-17 14:13:40 +0100

    Try catch block on notification proccess in succes controller

--- Controller/Checkout/Success.php
+++ Controller/Checkout/Success.php
@@ -5,14 +5,11 @@ namespace Paynow\PaymentGateway\Controller\Checkout;
 use Magento\Checkout\Model\Session as CheckoutSession;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
-use Magento\Framework\App\ResponseInterface;
 use Magento\Framework\Controller\Result\Redirect as ResponseRedirect;
-use Magento\Framework\Controller\ResultInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Sales\Model\Order;
 use Paynow\PaymentGateway\Helper\NotificationProcessor;
 use Paynow\PaymentGateway\Helper\PaymentField;
-use Paynow\PaymentGateway\Helper\PaymentHelper;
 use Paynow\PaymentGateway\Helper\PaymentStatusService;
 use Paynow\PaymentGateway\Model\Logger\Logger;
 
@@ -42,20 +39,11 @@ class Success extends Action
      * @var Order
      */
     private $order;
-    /**
-     * @var \Magento\Framework\App\RequestInterface
-     */
-    private $request;
 
     /**
      * @var UrlInterface
      */
     protected $urlBuilder;
-
-    /**
-     * @var PaymentHelper
-     */
-    private $paymentHelper;
 
     /**
      * @var NotificationProcessor
@@ -74,7 +62,6 @@ class Success extends Action
      * @param Logger $logger
      * @param UrlInterface $urlBuilder
      * @param NotificationProcessor $notificationProcessor
-     * @param PaymentHelper $paymentHelper
      * @param PaymentStatusService $paymentStatusService
      */
     public function __construct(
@@ -83,25 +70,22 @@ class Success extends Action
         Logger $logger,
         UrlInterface $urlBuilder,
         NotificationProcessor $notificationProcessor,
-        PaymentHelper $paymentHelper,
         PaymentStatusService $paymentStatusService
     ) {
         parent::__construct($context);
         $this->checkoutSession = $checkoutSession;
         $this->logger = $logger;
         $this->redirectResult = $this->resultRedirectFactory->create();
-        $this->request = $this->getRequest();
         $this->urlBuilder = $urlBuilder;
         $this->notificationProcessor = $notificationProcessor;
-        $this->paymentHelper = $paymentHelper;
         $this->order = $this->checkoutSession->getLastRealOrder();
         $this->paymentStatusService = $paymentStatusService;
     }
 
     /**
-     * @return ResponseInterface|ResponseRedirect|ResultInterface
+     * @return ResponseRedirect
      */
-    public function execute()
+    public function execute(): ResponseRedirect
     {
         $isRetry = $this->order &&
             $this->order->getPayment() &&
@@ -133,7 +117,12 @@ class Success extends Action
         $allPayments = $this->order->getAllPayments();
         $lastPaymentId = end($allPayments)->getAdditionalInformation(PaymentField::PAYMENT_ID_FIELD_NAME);
         $status = $this->paymentStatusService->getPaymentStatus($lastPaymentId);
-        $this->notificationProcessor->process($lastPaymentId, $status, $this->order->getIncrementId());
+        $loggerContext = [PaymentField::PAYMENT_ID_FIELD_NAME => $lastPaymentId];
+        try {
+            $this->notificationProcessor->process($lastPaymentId, $status, $this->order->getIncrementId());
+        } catch (\Exception $exception) {
+            $this->logger->error($exception->getMessage(), $loggerContext);
+        }
     }
 
     /**
