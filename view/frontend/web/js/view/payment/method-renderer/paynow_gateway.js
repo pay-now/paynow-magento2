a75932dd05e6ed99bae2b7352a6c7c8ca85f4473
Emil Leszczak <emil.leszczak@intramedium.pl>
2022-04-05 13:22:45 +0200

    Feature/change gateway label (#70)
    Change paynow gateway label

--- view/frontend/web/js/view/payment/method-renderer/paynow_gateway.js
+++ view/frontend/web/js/view/payment/method-renderer/paynow_gateway.js
@@ -69,6 +69,9 @@ define(
             isButtonActive: function () {
                 return this.getCode() === this.isChecked();
             },
+            isBlikGatewayActive: function () {
+              return window.checkoutConfig.payment.paynow_blik_gateway.isActive;
+            },
             setPaymentMethod: function (paymentMethod) {
                 if (paymentMethod.enabled) {
                     this.paymentMethodId = paymentMethod.id;
