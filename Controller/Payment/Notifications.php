0dbe5a02f18a41fc462a6c0ff182da80da407843
mikolajpodkonski <mikolaj.podkonski@movecloser.pl>
2022-09-19 18:34:07 +0200

    isCorrectStatus stricted to same paymentId added

--- Controller/Payment/Notifications.php
+++ Controller/Payment/Notifications.php
@@ -90,9 +90,7 @@ class Notifications extends Action
     {
         $payload          = $this->getRequest()->getContent();
         $notificationData = json_decode($payload, true);
-        if (!is_null($notificationData)){
-            $this->logger->debug("Received payment status notification", $notificationData);
-        }
+        $this->logger->debug("Received payment status notification", $notificationData);
         $storeId      = $this->storeManager->getStore()->getId();
         $signatureKey = $this->configHelper->getSignatureKey($storeId, $this->configHelper->isTestMode($storeId));
 
