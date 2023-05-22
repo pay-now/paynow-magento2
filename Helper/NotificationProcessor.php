aa0c63e277ee5c1ded4ac61f6d3a68ac516fadfe
mikolajpodkonski <mikolaj.podkonski@movecloser.pl>
2023-05-22 15:05:18 +0200

    Refactor

--- Helper/NotificationProcessor.php
+++ Helper/NotificationProcessor.php
@@ -249,8 +249,9 @@ class NotificationProcessor
             $this->logger->debug('Notification processed successfully', $this->context);
         }
         $this->lockingHelper->delete($externalId);
-        $this->eventManager->dispatch('paynow_paymentgateway_notification_processed',
-            ['order' => $order, 'paynowPaymentStatus' => $status]);
+        $this->eventManager->dispatch(
+            'paynow_paymentgateway_notification_processed', ['order' => $order, 'paynowPaymentStatus' => $status]
+        );
     }
 
     /**
