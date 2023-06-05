2fd03fb76ba6a4ea48b9d755aeb4dfd561686680
mikolajpodkonski <mikolaj.podkonski@movecloser.pl>
2023-06-05 10:30:30 +0200

    Translation + validator js fix

--- view/adminhtml/web/js/admin-config/validator-rules-mixin.js
+++ view/adminhtml/web/js/admin-config/validator-rules-mixin.js
@@ -7,7 +7,7 @@ define([
             'validate-api-credentials',
             function (value) {
                 let thisRegex = new RegExp(/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/);
-                return !(thisRegex.test(value));
+                return thisRegex.test(value);
             },
             $.mage.__('Incorrect API/Signature key format.')
         );
